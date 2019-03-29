e9e7e745800448fa05133242bfe5726751093ffc
liubich <liubich.a@gmail.com>
2019-03-30 01:00:50 +0200

    Fix eslint issues

--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
+class Card {
+  constructor(number, count, clickListener) {
+    const img = document.createElement('img');
+    img.src = 'img/js-badge.svg';
+    img.alt = number;
+    img.id = `img${number}`;
+    img.order = Math.random() * 100;
+    img.classList.add(`cards${count}`, 'card');
+    img.addEventListener('click', clickListener);
+    this.clickListener = clickListener;
+    this.img = img;
+    this.number = number;
+    this.opened = false;
+    this.visible = true;
+  }
+
+  open() {
+    this.img.src = `img/${this.fileNumber}.svg`;
+    this.opened = true;
+  }
+
+  close() {
+    this.img.src = 'img/js-badge.svg';
+    this.opened = false;
+  }
+
+  hide() {
+    this.img.classList.add('hidden');
+    this.visible = false;
+    this.opened = false;
+    this.img.removeEventListener('click', this.clickListener);
+  }
+
+  setFileNumber(fileNumber) {
+    this.fileNumber = fileNumber;
+  }
+}
 class Timer {
   constructor() {
     this.started = false;
@@ -5,6 +42,7 @@ class Timer {
 
   startTimer() {
     const pageTimer = document.getElementById('timer');
+    pageTimer.classList.remove('hidden');
     let numSec = 0;
     this.intStarted = setInterval(() => {
       numSec += 1;
@@ -27,109 +65,115 @@ class Timer {
     return this.started;
   }
 }
-class DOMManager {
-  constructor() {
-    this.cardsCountVar = document.getElementsByClassName('card').length;
-    this.clickListenerBinded = this.clickListener.bind(this);
-    this.imgs = [];
-    for (let i = 0; i < this.cardsCountVar; i += 1) {
-      this.imgs.push(document.getElementById(`img${i}`));
-    }
-  }
-
-  setListeners(imageOnClick) {
-    this.imageOnClick = imageOnClick;
-    this.imgs.forEach((img) => {
-      img.addEventListener('click', this.clickListenerBinded);
-    });
-  }
-
-  clickListener(event) {
-    const idClicked = parseInt(event.target.id.slice(3), 10);
-    this.imageOnClick(idClicked);
-  }
-
-  get cardsCount() {
-    return this.cardsCountVar;
-  }
-
-  showFace(cardNumber, srcFileNumber) {
-    this.imgs[cardNumber].src = `img/${srcFileNumber}.svg`;
-  }
-
-  showBack(cardNumber) {
-    this.imgs[cardNumber].src = 'img/js-badge.svg';
-  }
-
-  hideCard(cardNumber) {
-    this.imgs[cardNumber].removeEventListener('click', this.clickListener);
-    this.imgs[cardNumber].classList.add('hidden');
-  }
-}
 class Game {
   constructor() {
     this.imageOnClickBinded = this.imageOnClick.bind(this);
-    this.imagesRandomised = [];
-    this.nowOpened = 0;
     this.timer = new Timer();
-    this.DOMManagerInst = new DOMManager();
-    this.DOMManagerInst.setListeners(this.imageOnClickBinded);
-    this.numCards = this.DOMManagerInst.cardsCount;
+  }
+
+  start(cards) {
+    this.cards = cards;
+    this.cardsCount = cards.length;
+    this.imagesRandomised = [];
     this.fillImagesRandom();
+    this.timer.startTimer();
   }
 
   fillImagesRandom() {
     const imageNumbers = [];
-    for (let i = 0; i < 6; i += 1) {
+    for (let i = 0; i < this.cardsCount / 2; i += 1) {
       imageNumbers.push(i);
       imageNumbers.push(i);
     }
-    for (let i = this.numCards - 1; i >= 0; i -= 1) {
+    for (let i = this.cardsCount - 1; i >= 0; i -= 1) {
       const num = Math.round(Math.random() * i);
       this.imagesRandomised.push(imageNumbers[num]);
       imageNumbers.splice(num, 1);
     }
+    let i = 0;
+    this.cards.forEach((card) => {
+      card.setFileNumber(this.imagesRandomised[i]);
+      i += 1;
+    });
   }
 
   imageOnClick(idClicked) {
-    if (!this.timer.isStarted) {
-      this.timer.startTimer();
-    }
     if (idClicked === this.prevClicked) return;
-    this.nowOpened += 1;
-    if (this.nowOpened > 2) {
-      this.nowOpened = 2;
-      return;
-    }
-    this.DOMManagerInst.showFace(idClicked, this.imagesRandomised[idClicked]);
-    if (this.nowOpened === 1) {
+    if (this.openedCards === 2) return;
+    this.cards[idClicked].open();
+    if (this.openedCards === 1) {
       this.prevClicked = idClicked;
       return;
     }
-    if (this.imagesRandomised[idClicked] !== this.imagesRandomised[this.prevClicked]) {
+    if (this.cards[this.prevClicked].fileNumber !== this.cards[idClicked].fileNumber) {
       setTimeout(() => this.returnBackImage(idClicked), 1000);
       return;
     }
     setTimeout(() => {
-      this.DOMManagerInst.hideCard(idClicked);
-      this.DOMManagerInst.hideCard(this.prevClicked);
+      this.cards[idClicked].hide();
+      this.cards[this.prevClicked].hide();
       this.prevClicked = undefined;
-      this.nowOpened = 0;
-      this.numCards -= 2;
-      if (!this.numCards) {
+      if (!this.visibleCards) {
         this.timer.stopTimer();
       }
     }, 300);
   }
 
   returnBackImage(idClicked) {
-    this.DOMManagerInst.showBack(idClicked);
-    this.DOMManagerInst.showBack(this.prevClicked);
+    this.cards[idClicked].close();
+    this.cards[this.prevClicked].close();
     this.prevClicked = undefined;
-    this.nowOpened = 0;
+  }
+
+  get openedCards() {
+    return this.cards.filter(a => a.opened).length;
+  }
+
+  get visibleCards() {
+    return this.cards.filter(a => a.visible).length;
+  }
+}
+
+
+class DOMManager {
+  constructor() {
+    this.clickListenerBinded = this.clickListener.bind(this);
+    this.submitOnClickBinded = this.submitOnClick.bind(this);
+    this.game = new Game();
+    document.getElementById('Easy').addEventListener('change', DOMManager.difficultOnChange);
+    document.getElementById('Medium').addEventListener('change', DOMManager.difficultOnChange);
+    document.getElementById('Hard').addEventListener('change', DOMManager.difficultOnChange);
+    document.getElementById('submit').addEventListener('click', this.submitOnClickBinded);
+  }
+
+  static difficultOnChange() {
+    document.getElementById('submit').disabled = false;
+  }
+
+  submitOnClick() {
+    const selectedDiff = document.querySelectorAll('input[name="difficulty"]:checked');
+    this.cardsCount = parseInt(selectedDiff[0].value, 10);
+    document.getElementById('modalContainer').classList.add('hidden');
+    const cards = this.createCards();
+    this.game.start(cards);
+  }
+
+  createCards() {
+    const fragment = document.createDocumentFragment();
+    const cards = Array(this.cardsCount)
+      .fill(0).map((_, index) => new Card(index, this.cardsCount, this.clickListenerBinded));
+    cards.forEach(card => fragment.appendChild(card.img));
+    const mainContainer = document.getElementById('cardsContainer');
+    mainContainer.appendChild(fragment);
+    return cards;
+  }
+
+  clickListener(event) {
+    const idClicked = parseInt(event.target.id.slice(3), 10);
+    this.game.imageOnClickBinded(idClicked);
   }
 }
 
 
 // eslint-disable-next-line no-new
-new Game();
+new DOMManager();
