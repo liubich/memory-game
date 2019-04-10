13680217ef1f1ef571dd29abe912535cc1fb1bfe
liubich <liubich.a@gmail.com>
2019-04-10 16:55:35 +0300

    More structured code

--- src/main.js
+++ src/main.js
@@ -83,6 +83,7 @@ class Game {
   constructor() {
     this.imageOnClickBinded = this.imageOnClick.bind(this);
     this.timer = new Timer();
+    this.fieldIsBlocked = false;
   }
 
   appendImages(cards) {
@@ -114,42 +115,65 @@ class Game {
   }
 
   imageOnClick(idClicked) {
-    if (idClicked === this.prevClicked) return;
-    if (this.openedCards === 2) return;
-    this.cards[idClicked].open();
-    if (this.openedCards === 1) {
-      this.prevClicked = idClicked;
+    if(this.fieldIsBlocked) return;
+    switch (this.openedCardsNum) {
+      case 0:
+        this.cards[idClicked].open();
+        return;
+      case 1:
+        if(this.isCardOpened(idClicked)) return;
+        this.cards[idClicked].open();
+        this.fieldIsBlocked = true;
+        this.checkCards();
+        return;
+      case 2:
+        return;
+    }
+  }
+
+  isCardOpened(cardId) {
+    return parseInt(this.openedCards[0].div.id.slice(3), 10) === cardId;
+  }
+
+  checkCards() {
+    if (!this.areCardsEqual()) {
+      setTimeout(() => this.returnBackImage(), 1000);
       return;
     }
-    if (!this.cardsEqual(this.prevClicked, idClicked)) {
-      setTimeout(() => this.returnBackImage(idClicked), 1000);
-      return;
+    setTimeout(() => this.hideGuessedCards(), 600);
+  }
+
+  areCardsEqual() {
+    const hiddenValuesOfOpened = this.cards.filter(card=>card.opened).map(card=>card.hiddenValue);
+    return hiddenValuesOfOpened[0] === hiddenValuesOfOpened[1];
+  }
+
+  returnBackImage() {
+    const openedCards = this.openedCards;
+    openedCards[0].close();
+    openedCards[1].close();
+    this.fieldIsBlocked = false;
+  }
+
+  hideGuessedCards() {
+    const openedCards = this.openedCards;
+    openedCards[0].hide();
+    openedCards[1].hide();
+    this.fieldIsBlocked = false;
+    if (!this.visibleCardsNum) {
+      this.timer.stopTimer();
     }
-    setTimeout(() => {
-      this.cards[idClicked].hide();
-      this.cards[this.prevClicked].hide();
-      this.prevClicked = undefined;
-      if (!this.visibleCards) {
-        this.timer.stopTimer();
-      }
-    }, 600);
-  }
+  };
 
-  cardsEqual(cardIndex1, cardIndex2) {
-    return this.cards[cardIndex1].hiddenValue === this.cards[cardIndex2].hiddenValue;
-  }
-
-  returnBackImage(idClicked) {
-    this.cards[idClicked].close();
-    this.cards[this.prevClicked].close();
-    this.prevClicked = undefined;
-  }
-
-  get openedCards() {
+  get openedCardsNum() {
     return this.cards.filter(a => a.opened).length;
   }
 
-  get visibleCards() {
+  get openedCards() {
+    return this.cards.filter(a => a.opened)
+  }
+
+  get visibleCardsNum() {
     return this.cards.filter(a => a.visible).length;
   }
 }
